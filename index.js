dfae8e4837fda41fdf05dfea12c7268d7fb03580
Diego Barrera <di_barrera@hotmail.com>
2021-01-25 13:50:04 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+Nombre: Soul Catcher
+
+Tres niveles de dificultad que aumentarian la generación de elementos random
+
+
+Instrucciones: 
+
+Esta es la historia de ::: 
+Su deber es el de llevar las almas bondadosas
+al más alla, ayuda a ::: a atrapar las almas buenas
+para ayudarlas en su transición
+
+Pero existen los youkai (espiritus corrompidos),
+::: tendra que esquivar estos espiritus corruptos o sino
+su alma sera corrompida
+
+Para mover a ::: utiliza los teclas de iaquierda y derecha
+Para saltar utiliza la barra de espacio
+
+::: Cuenta con tres corazones, no dejes que pierda todos sus corazones,
+esquivando los espiritus
+
+*/
